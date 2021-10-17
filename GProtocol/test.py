da6a283e3c3d8c68daa61e916966d99cfc337bff
x3beche <x3beche@gmail.com>
2021-10-17 19:43:24 +0300

    Update test.py

--- GProtocol/test.py
+++ GProtocol/test.py
@@ -2,6 +2,7 @@ from ghostprotocol import nrf24l01,portChecker
 from random import randint
 from time import sleep
 
+# Port Selection
 ports = portChecker()
 for i,port in enumerate(ports) : print(f"{i+1}-) {port}")
 port = ports[int(input("Select Port : "))-1]
@@ -12,8 +13,15 @@ print(m.cardSpec) # See the properties of the object
 
 
 while True:
-    data = input("Mesaj Giriniz >> ")
-    #data = str(randint(0,10**2000))
+    
+    # Transmitting Mode
+    data = input("Enter the message to be sent >> ")
     s,f,t = m.tx(data).values()
     print(f"Total Success : {m.totalCorrectTransmissions} | Total Failed {m.totalIncorrectTransmissions} | Success : {s} | Failed {f} | Time Elapsed : {t}")
 
+    # Receiving Mode
+    """
+    cache = m.rx()
+    if cache != None:
+        print(f">> {cache}")
+    """
